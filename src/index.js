dcdec98f12a2b4dbaba89fafb80f17c39a3a3005
David Vidal <gvd@Davids-MacBook-Pro.local>
2019-09-30 01:29:32 -0500

    Solucion reto 05 - problema 1,2,3,4

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://rickandmortyapi.com/api/character/';
 
+clearLocalStorage = () => {
+  localStorage.clear();
+}
+
 const getData = api => {
   fetch(api)
     .then(response => response.json())
     .then(response => {
+      var info = response.info;
+      localStorage.setItem('next_fetch',info.next)
       const characters = response.results;
       let output = characters.map(character => {
         return `
@@ -23,8 +29,23 @@ const getData = api => {
     .catch(error => console.log(error));
 }
 
-const loadData = () => {
-  getData(API);
+const loadData = async () => {
+  try{
+    var API_NEXT_PAGE = localStorage.getItem('next_fetch');
+    if(API_NEXT_PAGE == null){
+      return await getData(API);
+    }else{
+      if(API_NEXT_PAGE == ''){
+        let newItem = document.createElement('p');      
+        newItem.innerHTML = "Ya no hay personajes...";
+        $app.appendChild(newItem);
+        intersectionObserver.unobserve($observe);
+      } else {
+        return await getData(API_NEXT_PAGE);
+      }
+    }
+  } catch {
+  }
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
